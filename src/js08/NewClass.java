c50908d3144983a6c1532ea1f856c84b50275479
Galih <Galih@LAPTOP-S8IDLHKL>
2018-08-02 11:13:59 +0700

    FOR, WHILE, DO.. WHILE.

---
+++ src/js08/NewClass.java
@@ -0,0 +1,24 @@
+package js08;
+
+public class NewClass {
+    public static void main(String[] args)
+    {
+     int x;
+     for (x = 1;x<= 5; x++){
+         System.out.println("SMK Telkom Malang");
+     }
+     int i =1;
+     while (i<=5)
+     {
+         System.out.println("SMK Telkom Malang");
+         i++;
+     }
+     int j=1;
+     do{
+         System.out.println("SMK Telkom Malang");
+         j++;
+     }
+     while (j<=5);
+         
+   }  
+}
